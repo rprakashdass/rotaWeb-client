2fc62956f3b1802ed345ecc0d6029b2abd9859ed
dharanidh75 <dharanidh777@gmail.com>
2024-10-07 14:53:25 +0530

    update home, events

--- src/Events/EventCard.jsx
+++ src/Events/EventCard.jsx
@@ -1,40 +1,46 @@
-
-import poster from '../assets/eventPoster.jpg'
+import poster from '../assets/eventPoster.jpg';
 
 const EventCard = ({ title, description }) => {
     return (
         <div className='container-fluid'>
             <div className="row">
-                <div className="col-md-7 justify-content-center align-items-center overflow-none">
-                    <div className="bg-dark" style={{objectFit:"contain", position: "relative"}} >
-                        <img 
+                <div className="col-md-5 d-flex justify-content-center align-items-center">
+                    <div className="align-items-center" style={{ maxWidth: '100%', borderRadius: '10px', }}>
+                        <img
                             src={poster}
                             alt='event poster'
-                            style={{position: "relative", width: "80%", height:"50%", margin:"30%"}}
+                            // margin: "10% 20%",
+                            style={{ margin: "0% 2% 2% 10%", maxWidth: "80%", height: "auto", borderRadius: '10px' }}
                         />
                     </div>
                 </div>
-                <div className="col-md-5">
-                    <h1>
-                        {title}
-                    </h1>
-                    <hr />
-                    <p>
-                        {description}
-                    </p>
+                <div className="col-md-7">
+                    <div className="card bg-light" style={{margin: "7%"}}>
+                        <div className="card-body ">
+                            <h1 className="card-title">{title}</h1>
+                            <hr />
+                            <h3>Description</h3>
+                            <p>{description}</p>
 
-                    <p className='alert alert-danger'>
-                        The website has been hosted is for the testing purposes hence please bareus incase of miscomforts.
-                        mail us incase of any technical issues <a href='mailto:rprakashdass@gmail.com'>here</a>
-                    </p>
+                            <hr />
 
-                    <button>
-                        Enter Quiz
-                    </button>
+                            <h3>Instructions</h3>
+                            <p className='alert alert-danger'>
+                                The website has been hosted for testing purposes. Please bear with us in case of any discomforts.
+                                Email us in case of any technical issues <a href='mailto:rprakashdass@gmail.com'>here</a>
+                            </p>
+
+                            <a className="btn btn-primary" href='
+                            https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAN__mh0hwdUOU9aVkMwTTNHMTdXQzI5MDhQMVc5NlNZVi4u&origin=QRCode
+                            '>
+                                Enter Quiz
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default EventCard;
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,26 +1,100 @@
+import img from './assets/favicon.png';
+import './styles/home.css';
+import { Link } from 'react-router-dom';
+
 const Home = () => {
     return (
         <>
-            <div className="container-fluid">
-                <h1 className="" style={{color: "#f57388"}}>Rotaract Club of Sri Shakthi Institute of Engineering & Technology</h1>
-                <p>
-                    This is an Test application which is running on onreader.com
-                </p>
-                <button className="btn btn-primary">
-                    <a href="/form" className="href">
-                        Get Started!.
-                    </a>
-                </button>
-                <div className="card">
-                    <div className="card-item">
-                        card 1
-                    </div>
-                    <div className="card-item">
-                        card 2
+            <div className="">
+                <div className="container-fluid p-5 card-bg-color">
+                    <h1 className="display-3" style={{ color: "#f57388" }}>Rotaract Club of Sri Shakthi Institute of Engineering & Technology</h1>
+                    <p className="lead">
+                        This is an Test application which is running on onreader.com
+                    </p>
+                </div>
+
+                {/* ongoing events */}
+
+                <div className='m-5'>
+                    <h3>Ongoing Events</h3>
+                    <div className='card bg-light' style={{maxWidth: "18rem"}}>
+                        <div className='card-header'>
+                            QR Desk
+                        </div>
+                        <p className='card-body'>
+                            A professional Service Initiative <br/>
+                                <Link to={'/qr-desk'}>
+                                    <button className='btn btn-primary' >
+                                        Attend the event
+                                    </button>
+                                </Link>
+                        </p>
                     </div>
                 </div>
+
+                {/* ongoing projects */}
+                <div className='m-5'>
+                    <h3>Ongoing Projects</h3>
+                    <div className='card bg-light' style={{maxWidth: "18rem"}}>
+                        <div className='card-header'>
+                            Arivukoodam
+                        </div>
+                        <p className='card-body'>
+                            A District Priority Project Initiative <br/>
+                            <button className='btn btn-primary'>Know more</button>
+                        </p>
+                    </div>
+                </div>
+
+                <div className='m-3 mt-5 row  justify-content-center gap-5'>
+                    {/*  */}
+                    <div className="col-md-2 card" style={{ width: "18rem" }}>
+                        <img className="card-img-top" src={img} alt="Card image cap" style={{ width: "100%", margin: "auto auto" }} />
+                        <div className="card-body">
+                            <p className="lead">Charter</p>
+                        </div>
+                    </div>
+
+                    {/*  */}
+                    <div className="card" style={{ width: "18rem" }}>
+                        <img className="card-img-top" src={img} alt="Card image cap" style={{ width: "100%", margin: "auto auto" }} />
+                        <div className="card-body">
+                            <p className="lead">Events</p>
+                        </div>
+                    </div>
+
+                    {/*  */}
+                    <div className="card" style={{ width: "18rem" }}>
+                        <img className="card-img-top" src={img} alt="Card image cap" style={{ width: "100%", margin: "auto auto" }} />
+                        <div className="card-body">
+                            <p className="lead">Awards</p>
+                        </div>
+                    </div>
+
+                    {/*  */}
+                    <div className="card" style={{ width: "18rem" }}>
+                        <img className="card-img-top" src={img} alt="Card image cap" style={{ width: "100%", margin: "auto auto" }} />
+                        <div className="card-body">
+                            <p className="lead">Recognitions</p>
+                        </div>
+                    </div>
+
+                    {/*  */}
+                    <div className="card" style={{ width: "18rem" }}>
+                        <img className="card-img-top" src={img} alt="Card image cap" style={{ width: "100%", margin: "auto auto" }} />
+                        <div className="card-body">
+                            <p className="lead">Members</p>
+                        </div>
+                    </div>
+
+                    <p className='alert alert-danger'>
+                                The website has been hosted for testing purposes. Please bear with us in case of any discomforts.
+                                Email us in case of any technical issues <a href='mailto:rprakashdass@gmail.com'>here</a>
+                    </p>
+
+                </div>
             </div>
-        </> 
+        </>
     )
 }
 
